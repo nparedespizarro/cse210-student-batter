2019cee9051d8a12a3a66b7a0da0c3a7bf780cec
nparedespizarro <nparedespizarro94@gmail.com>
2021-11-06 12:08:36 +0000

    Second Commit

---
+++ batter/game/play.py
@@ -0,0 +1,14 @@
+class Play:
+
+    def __init__(self):
+        self._keep_playing = True
+        self._message = "You are terrible"
+
+    def stop_play(self):
+        self._keep_playing = False
+    def get_play_state(self):
+        return self._keep_playing
+    def print_message(self):
+        print(self._message)
+    def get_message(self):
+        return self._message
