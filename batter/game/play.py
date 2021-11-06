41cb2520b1d5bedb7b7d8aee4c24c66b83042943
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2021-11-06 17:44:08 -0300

    Added comments
    Added comments

--- batter/game/play.py
+++ batter/game/play.py
@@ -1,14 +1,26 @@
 class Play:
+    """The class play dicides if the game should cotinue or not.
+    
+    Stereotype:
+        'Terminator'
+        
+    Attributes:
+        _keep_playing: An boolean value.
+        _message: An string."""
 
     def __init__(self):
         self._keep_playing = True
         self._message ="YOU LOSE"
 
     def stop_play(self):
+        """"This function stops the game."""
         self._keep_playing = False
     def get_play_state(self):
+        """"This function keeps the game running."""
         return self._keep_playing
     def print_message(self):
+        """Prints the message."""
         print(self._message)
     def get_message(self):
+        """"Gets the message."""
         return self._message
