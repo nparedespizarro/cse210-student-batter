fda0affa77ef50fb7eb3c41228a7378fef4a67d2
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2021-11-06 17:51:32 -0300

    Added comments for this class
    Added comments for this class

--- batter/game/draw_actors_action.py
+++ batter/game/draw_actors_action.py
@@ -1,12 +1,21 @@
 from game.action import Action
 
-# TODO: Define the DrawActorsAction class here
 class DrawActorsAction:
+    """Draw the actors(bricks, ball, and bat) and also draw the actions(movement and placement).
+    
+    Steriotype:
+        Responsive for the visual.
+        
+    Attributes:
+        _output_service: The output_service class."""
     
     def __init__(self, output_service):
+         """The constructor function."""
         
         self._output_service = output_service
+        
     def execute(self, cast):
+        """This function executes and tells the program to start drawing."""
         self._output_service.clear_screen()
         for i in cast:
             self._output_service.draw_actors(cast[i])
